1913cbcbeeae49a7068eef64966a6be1f158185e
wafd222 <wafd.almadinah@gmail.com>
2026-07-11 15:37:51 +0300

    Initial Wafd One App

---
+++ wafd_one/wafd_one/hooks.py
@@ -0,0 +1,7 @@
+app_name = "wafd_one"
+app_title = "WAFD ONE"
+app_publisher = "Wafd Almadinah"
+app_description = "Catering and delivery operations for ERPNext"
+app_email = "wafd.almadinah@gmail.com"
+app_license = "proprietary"
+after_install = "wafd_one.install.after_install"
---
+++ wafd_one/wafd_one/wafd_one/doctype/wafd_hotel/wafd_hotel.py
@@ -0,0 +1,4 @@
+from frappe.model.document import Document
+
+class WafdHotel(Document):
+    pass
